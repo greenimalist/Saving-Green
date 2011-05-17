a78b0c27113c1a6eb5d2017aa5176c10a3f62997
Aaron Lin <aaron.j.lin@gmail.com>
2011-05-17 08:10:13 +0800

    Implemented NSCoding for EnergyBuster
    Easy archiving

--- EnergySaver/EnergyBuster.m
+++ EnergySaver/EnergyBuster.m
@@ -47,5 +47,47 @@
     return savingsPerMonth * 12;
 }
 
+// NSCoding Methods
+
+- (id)initWithCoder:(NSCoder *)decoder {
+//  NSObject doesn't conform to NSCoder
+// self = [super initWithCoder:decoder];
+    
+    self = [self init];
+    
+    self.title = [decoder decodeObjectForKey:@"Title"];
+    self.description = [decoder decodeObjectForKey:@"Description"];
+    self.choice = [decoder decodeObjectForKey:@"Choice"];
+    self.graphicURL = [decoder decodeObjectForKey:@"Graphic URL"];
+    self.eventName = [decoder decodeObjectForKey:@"Event Name"];
+    self.eventsPerMonth = [decoder decodeIntForKey:@"Events Per Month"];
+    self.savingsPerEvent = [decoder decodeIntForKey:@"Savings Per Event"];
+    self.savingsPerMonth = [decoder decodeIntForKey:@"Savings Per Month"];
+    self.setupCost = [decoder decodeIntForKey:@"Setup Cost"];
+    self.setupTime = [decoder decodeIntForKey:@"Setup Time"];
+    self.ongoingTime = [decoder decodeIntForKey:@"Ongoing Time"];
+    self.startDate = [decoder decodeObjectForKey:@"Start Date"];
+
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)encoder {
+//  NSObject doesn't conform to NSCoder
+//  [super encodeWithCoder:encoder];
+
+    [encoder encodeObject:title forKey:@"Title"];
+    [encoder encodeObject:description forKey:@"Description"];
+    [encoder encodeObject:choice forKey:@"Choice"];
+    [encoder encodeObject:graphicURL forKey:@"Graphic URL"];
+    [encoder encodeObject:eventName forKey:@"Event Name"];
+    [encoder encodeInt:eventsPerMonth forKey:@"Events Per Month"];
+    [encoder encodeInt:savingsPerEvent forKey:@"Savings Per Event"];
+    [encoder encodeInt:savingsPerMonth forKey:@"Savings Per Month"];
+    [encoder encodeInt:setupCost forKey:@"Setup Cost"];
+    [encoder encodeInt:setupTime forKey:@"Setup Time"];
+    [encoder encodeInt:ongoingTime forKey:@"Ongoing Time"];
+    [encoder encodeObject:startDate forKey:@"Start Date"];
+}
 
 @end
